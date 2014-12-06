5f7464845bee3447ac9c562988e3d2a70c5ea150
herious1989 <lamhnguyen@csupomona.edu>
2014-12-05 22:58:56 -0800

    finalizing

--- src/com/example/ptevalform/Client.java
+++ src/com/example/ptevalform/Client.java
@@ -1,7 +1,5 @@
 package com.example.ptevalform;
 
-import java.util.ArrayList;
-
 public class Client {
 	private String mPTName;
 	String mFirstName;
@@ -10,8 +8,8 @@ public class Client {
 	private int mClientID;
 	private String mDOB;
 	private int mAge; 	
-	private double mHeight;
-	private double mWeight;
+	private String mHeight;
+	private String mWeight;
 	private String mPhoneNumber;
 	private String mEmployerName;
 	private boolean mIsEmployed;
@@ -22,6 +20,11 @@ public class Client {
 				String xLastName, 
 				String xOccupation,
 				String xDOB,
+				int xAge,
+				String xHeight,
+				String xWeight,
+				String xPhoneNumber,
+				String xEmployerName,
 				String xPTName) {
 		mClientID = xClientID;
 		mPTName = xPTName;
@@ -29,6 +32,11 @@ public class Client {
 		mLastName = xLastName;
 		mOccupation = xOccupation;
 		mDOB = xDOB;
+		mAge = xAge;
+		mHeight = xHeight;
+		mWeight = xWeight;
+		mPhoneNumber = xPhoneNumber;
+		mEmployerName = xEmployerName;
 	}
 
 	public int getClientID() {
@@ -87,19 +95,19 @@ public class Client {
 		mAge = xAge;
 	}
 	
-	public double getHeight() {
+	public String getHeight() {
 		return mHeight;
 	}
 		
-	public void setHeight(double xHeight) {
+	public void setHeight(String xHeight) {
 		mHeight = xHeight;
 	}
 	
-	public double getWeight() {
+	public String getWeight() {
 		return mWeight;
 	}
 		
-	public void setWeight(double xWeight) {
+	public void setWeight(String xWeight) {
 		mWeight = xWeight;
 	}
 	
